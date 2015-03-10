18e4193544ea457c19f6d0e96b53b8bf532b8360
Fernando Mendez <fernando.a.mendez69@gmail.com>
2015-03-09 21:28:04 -0400

    Add the append to word practice

---
+++ general_editing/delete_into_black_hole_register.rb
@@ -0,0 +1,5 @@
+# The "_d{motion} deletes into the black hole register, that way you avoid replacing whats currently copied into the 
+# register
+#
+#
+# Practice text
---
+++ general_editing/paste_after_before_cursor.rb
@@ -0,0 +1,7 @@
+# p is use to paste yaked text AFTER the cursor 
+# P is use to paste yaked text BEFOR the cursor 
+#
+#
+#
+# Practice text:
+
