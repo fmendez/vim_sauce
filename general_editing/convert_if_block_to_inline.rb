4faf9b16717d734c30c0239d699496340fa8c603
Fernando Mendez <fernando.a.mendez69@gmail.com>
2015-03-10 08:14:07 -0400

    Add more tests

---
+++ general_editing/convert_if_block_to_inline.rb
@@ -0,0 +1,71 @@
+# Convert if block into inline
+#
+
+
+if condition
+  thing_to_execute
+end
+
+if condition
+  thing_to_execute
+end
+
+if condition
+  thing_to_execute
+end
+
+if condition
+  thing_to_execute
+end
+
+if condition
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
+
+if condition && another_condition || something_else
+  thing_to_execute
+end
--- general_editing/delete_into_black_hole_register.rb
+++ general_editing/delete_into_black_hole_register.rb
@@ -1,8 +1,9 @@
 # The "_d{motion} deletes into the black hole register, that way you avoid replacing whats currently copied into the 
 # register
 #
+# Another way, is to use register 0 with "0p when pasting. That register will always contain the 
+# last character yanked
 #
-# Practice text
 
 to_be_copied
 
